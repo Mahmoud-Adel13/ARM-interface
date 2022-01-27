4c2e53b39bab42a9e450dc1499417990dc34e8f5
Mahmoud-Adel13 <eng-mahmoud.adel1621@alexu.edu.eg>
2022-01-27 17:24:47 +0200

    MSTK commit

--- BPC/MSTK/STK_program.c
+++ BPC/MSTK/STK_program.c
@@ -12,6 +12,10 @@
 #include  "MSTK_private.h"
 #include  "MSTK_config.h"
 
+/* Global variable */
+u8 Global_u8PeriodicState;
+
+
 /* Functions definition part: */
 void MSTK_voidInit(void)
 {
@@ -34,7 +38,7 @@ void MSTk_voidSetBusyWait(u32 Copy_u32Ticks)
   /* Clear Value Register */
 	STK-> VAL  = 0 ;
   /* Set time of uP delay */
-  STK-> LOAD = Copy_u32Ticks;
+  STK-> LOAD = Copy_u32Ticks - 1;
   /* Disable SysTick interrupt: */
   CLR_BIT(CTRL , 1);
   /* SysTick enable */
@@ -47,17 +51,13 @@ void MSTK_voidSetIntervalSignal(u32 Copy_u32Ticks , void (*Copy_ptr) (viod))
   /* Clear Value Register */
 	STK-> VAL  = 0 ;
   /* Set interval duration */
-  STK-> LOAD = Copy_u32Ticks;
+  STK-> LOAD = Copy_u32Ticks - 1;
   /* Enable SysTick interrupt: */
   SET_BIT(CTRL , 1);
   /* SysTick enable */
   SET_BIT(CTRL , 0);
   MSTK_Callback = Copy_ptr;                                                     /* Callback function assignment */
-  if(STK->VALUE == 0x0)                                                         /* After interval ends stop counting */
-  {
-    STK-> LOAD  = 0x0000;
-    STK-> VALUE = 0x0000;
-  }
+  Global_u8PeriodicState = 0;
 }
 
 void MSTK_voidSetIntervalPeriodic(u32 Copy_u32Ticks , void (*Copy_ptr) (viod))
@@ -65,12 +65,13 @@ void MSTK_voidSetIntervalPeriodic(u32 Copy_u32Ticks , void (*Copy_ptr) (viod))
   /* Clear Value Register */
 	STK-> VAL  = 0 ;
   /* Set interval duration */
-  STK-> LOAD = Copy_u32Ticks;
+  STK-> LOAD = Copy_u32Ticks - 1;
   /* Enable SysTick interrupt: */
   SET_BIT(CTRL , 1);
   /* SysTick enable */
   SET_BIT(CTRL , 0);
   MSTK_Callback = Copy_ptr;                                                     /* Callback function assignment */
+  Global_u8PeriodicState = 1;
 }
 
 void MSTK_voidStopInterval(void)
@@ -81,12 +82,14 @@ void MSTK_voidStopInterval(void)
 
 u32 MSTK_u32FGetElapsedTime(void)
 {
+  STk->VALUE &= ~(0xFF000000);                                                  /* Setting reserved bits as zeros */
   u32  Local_u32Elapsed = (STK->LOAD) - (STK->VALUE);
   return Local_u32Elapsed;
 }
 
 u32 MSTK_u32FGetRemainingTime(void)
 {
+  STk->VALUE &= ~(0xFF000000);                                                  /* Setting reserved bits as zeros */
   u32  Local_u32Elapsed = (STK->VALUE);
   return Local_u32Elapsed;
 }
@@ -96,4 +99,9 @@ u32 MSTK_u32FGetRemainingTime(void)
 void SysTick_Handler(viod (*MSTK_Callback)(void))
 {
   *MSTK_Callback();
+  if(Global_u8PeriodicState == 0)
+  {
+    STK-> LOAD  = 0x0000;
+    STK-> VALUE = 0x0000;
+  }
 }
